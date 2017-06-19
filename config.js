01c84c3309363544aa0c70ac3edb51552a109581
Yehonal <yehonal.azeroth@gmail.com>
2017-06-19 17:45:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
   var app = angular.module('apawviewer');
 
   /* Edit with path of Apaw Viewer */
-  app.api = "http://azerothshard.org/modules/apaw/web/";
+  app.api = location.protocol+"//azerothshard.org/modules/apaw/web/";
 
 })();
